8fa13ffc6e78d2585f9fa648871e5dcc92bf3ab7
tccmw <163850138+tccmw@users.noreply.github.com>
2024-04-26 16:10:29 +0900

    Add files via upload

---
+++ 기초 연산자.cpp
@@ -0,0 +1,156 @@
+//1 연산자
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a,b;
+//	scanf("%d %d",&a,&b ); 
+//	printf("두 개의 수를 입력하시오\n ");
+//	
+//	printf("%d + %d = %d\n", a,b,a+b);
+//	printf("%d - %d = %d\n", a,b,a-b);
+//	printf("%d * %d = %d\n", a,b,a*b);
+//	printf("%d / %d = %d\n", a,b,a/b);
+//	printf("%d %% %d = %d\n", a,b,a%b);
+// } 
+
+//연산자 형성평가 1
+//#include <stdio.h>
+//
+//int main(){
+//	int a,b,c,d;
+//	scanf("%d %d %d %d", &a, &b, &c, &d);
+//	printf("sum %d\n", a+b+c+d);
+//	printf("avg %d", (a+b+c+d)/4);
+//	return 0;
+//}
+
+//연산자  형성평가 2
+//#include <stdio.h>
+//
+//int main(){
+//	int a,b;
+//	scanf("%d %d",&a, &b);
+//	printf("%d / %d = %d...%d", a, b, a/b, a%b);
+//	return 0;
+//} 
+
+//연산자 형성평가 3
+//#include <stdio.h>
+//
+//int main(){
+//	int a,b;
+//	scanf("%d %d",&a,&b);
+//	printf("width = %d\n", a+5);
+//	printf("length = %d\n", b*2);
+//	printf("area = %d", (a+5)*(b*2));
+//} 
+
+//연산자 형성평가 4
+//#include <stdio.h>
+//
+//int main(){
+//	int a,b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d %d\n",++a, b--);
+//	printf("%d %d", a,b);
+//} 
+
+//연산자 형성평가 5
+//#include <stdio.h>
+// 
+//int main(){
+//	int a, A, b, B;
+//	scanf("%d %d %d %d", &a, &A, &b, &B);
+//	int c=(a > b && A > B);
+//	printf("%d", c);
+//	
+//////	if(a > b){
+//////		if(A > B){
+//////			printf("1");
+//////		}
+//////	}
+//////	else{
+//////		printf("0");
+//////	}
+//	return 0;
+//} 
+
+//연산자 7
+//#include <stdio.h>
+//
+//int main(){
+//	int a=0,b=1,c=3;
+//	int r1=(a&&b);
+//	int r2=(a||b);
+//	int r3=(b&&c);
+//	int r4=!a;
+//	
+//	printf("%d %d %d %d\n",r1,r2,r3,r4);
+//	
+//	return 0;
+//} 
+
+//#include <stdio.h>
+// 
+//int main()
+//{
+//    int n;
+//    scanf("%d", &n);
+//    printf("%d", n);
+//
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(){
+//	char a;
+//	scanf("%c", &a);
+//	printf("%c",a);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(){
+//	double a;
+//	scanf("%lf",&a);
+//	printf("%f",a);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(){
+//	int a,b;
+//	scanf("%d %d", &a,&b);
+//	printf("%d %d", a,b);
+//	return 0;
+//}
+
+//#include <stdio.h>
+// int main(){
+// 	char a,b;
+// 	scanf("%c %c", &a, &b);
+// 	printf("%c %c",b,a);
+// 	return 0;
+// }
+
+//#include <stdio.h>
+//
+//int main(){
+//	double a;
+//	scanf("%lf",&a);
+//	printf("%.2f",a);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(){
+//	int a;
+//	scanf("%d", &a);
+//	printf("%d %d %d", a,a,a);
+//}
+
